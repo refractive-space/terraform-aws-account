0e51ee6b7d73c3ab4da8f855441ec67a0a260569
Alex Talbot <alex@refractive.space>
2025-09-21 19:57:50 +0100

    fix: Rework Cost Filters

--- main.tf
+++ main.tf
@@ -55,8 +55,14 @@ resource "aws_budgets_budget" "account_budget" {
   time_unit    = var.budget_time_unit
   time_period_start = formatdate("YYYY-MM-01_00:00", timestamp())
 
-  cost_filters = length(var.budget_cost_filters) > 0 ? var.budget_cost_filters : {
-    LinkedAccount = [aws_organizations_account.this.id]
+  dynamic "cost_filter" {
+    for_each = length(var.budget_cost_filters) > 0 ? var.budget_cost_filters : {
+      LinkedAccount = [aws_organizations_account.this.id]
+    }
+    content {
+      name   = cost_filter.key
+      values = cost_filter.value
+    }
   }
 
   dynamic "notification" {
